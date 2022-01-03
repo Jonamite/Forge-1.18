64800609f12a013b88aaf5dc7dcf81751badf0f5
Jonamite <jonamitegaming@gmail.com>
2022-01-02 20:01:05 -0500

    .

---
+++ src/main/java/com/jonamite/dense/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.jonamite.dense.item;
+
+import com.jonamite.dense.Dense;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Dense.MOD_ID);
+
+    public static final RegistryObject<Item> CHAPSTICK = ITEMS.register("chapstick",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
